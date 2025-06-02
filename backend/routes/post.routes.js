4eb947a50264e6f2409ec370d2f87ffc9ab6b38e
Fanny Simon <contact.fanny.simon@gmail.com>
2025-06-02 13:09:34 +0200

    tests des requêtes CRUD avec le server

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router(); //On est dans l'objet router de la biliothèque express
+
+// CRUD = Create, Read, Update, Delete (= get, post, put, delete)
+
+//On crée une route pour récupérer tous les posts
+router.get("/", (req, res) => {
+  res.json({ message: "Voici tous les posts" });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ message: req.body.message });
+});
+
+// On crée une route pour modifier un post (mise à jour)
+router.put("/:id", (req, res) => {
+  // console.log(req.params.id);
+  res.json({ messageId: `Voici le post ${req.params.id}` });
+});
+
+// On crée une route pour supprimer un post
+router.delete("/:id", (req, res) => {
+  res.json({ message: `Post supprimé avec succès ${req.params.id}` });
+});
+
+// On crée une route pour liker un post
+router.patch("/like-post/:id", (req, res) => {
+  res.json({ message: `Post liké: id: ${req.params.id}` });
+});
+
+// On crée une route pour ne pas liker un post
+router.patch("/dislike-post/:id", (req, res) => {
+  res.json({ message: `Post non liké: id: ${req.params.id}` });
+});
+
+//On exporte le router (tout notre projet aura accès à cette route)
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,20 @@ const port = 5000;
 
 const app = express();
 
-// req = request = la requête que nous envoyons au serveur
-// res = response = la réponse que nous recevons du serveur
-app.get("/post", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+// // req = request = la requête que nous envoyons au serveur
+// // res = response = la réponse que nous recevons du serveur
+// app.get("/post", (req, res) => {
+//   res.json({ message: "Hello World" });
+// });
+
+// Middleware pour parser le JSON (traiter les données de la requête)
+app.use(express.json());
+
+// Middleware pour parser les données de la requête (urlencoded)
+app.use(express.urlencoded({ extended: false }));
+
+// On utilise le router pour les posts
+app.use("/post", require("./routes/post.routes"));
 
 // Pour lancer le serveur (sur le numéro de port 5000 = en local car c'est au localhost 5000)
 app.listen(port, () => {
