863241587448d101a0ac039e944cbb5102c01442
Fanny Simon <contact.fanny.simon@gmail.com>
2025-06-03 10:43:41 +0200

    DELETE, LIKE, DISLIKE (suite du CRUD)

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,105 @@
 const PostModel = require("../models/post.model");
 
-//fonction qui c'exporte automatiquement:
+// CRUD = Create, Read, Update, Delete = get, post, put, delete
+
+// =======================READ (GET)=======================================
+
+// fonction qui s'exporte automatiquement:
+// controller pour lister les posts
+module.exports.getPosts = async (req, res) => {
+  const posts = await PostModel.find();
+  res.status(200).json(posts);
+};
+
+// =======================CREATE (POST)=======================================
+
+// controller pour créer un post
+// Ne pas oublier de le mettre dans le post.routes.js !
 module.exports.setPosts = async (req, res) => {
-  res.json({ message: "Post créé avec succès" });
+  if (!req.body.message) {
+    res.status(400).json({ message: "Message is required" });
+  }
+  // création d'un post
+  const post = await PostModel.create({
+    message: req.body.message,
+    author: req.body.author,
+  });
+  res.status(200).json(post);
+};
+
+// =======================UPDATE (PUT)=======================================
+
+// controller pour modifier un post
+// Ne pas oublier de le mettre dans le post.routes.js !
+module.exports.editPost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+  if (!post) {
+    res.status(400).json({ message: "Post not found" });
+  }
+
+  const updatedPost = await PostModel.findByIdAndUpdate(post, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedPost);
+};
+// post = (l'id du post que nous voulons modifier)
+// req.body = (les données que nous voulons modifier)
+// { new: true } = (nous retourne le post modifié)
+
+// =======================DELETE (DELETE)=======================================
+
+// controller pour supprimer un post
+// Ne pas oublier de le mettre dans le post.routes.js !
+module.exports.deletePost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+  if (!post) {
+    res.status(400).json({ message: "Post not found" });
+  }
+  await post.deleteOne();
+  res.status(200).json({ message: `Post deleted ${req.params.id}` });
+};
+
+// =======================LIKE (PATCH)=======================================
+
+// controller pour liker un post
+// Ne pas oublier de le mettre dans le post.routes.js !
+module.exports.likePost = async (req, res) => {
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $addToSet: {
+          likers: req.body.userId,
+        },
+      },
+      { new: true } //pour ne pas bloquer la BD
+    ).then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// =======================DISLIKE (PATCH)=======================================
+
+// controller pour ne pas liker un post
+// Ne pas oublier de le mettre dans le post.routes.js !
+module.exports.dislikePost = async (req, res) => {
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        // On retire l'utilisateur de la liste des likers:
+        $pull: {
+          likers: req.body.userId,
+        },
+      },
+      { new: true } //pour ne pas bloquer la BD
+    ).then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router(); //On est dans l'objet router de la biliothèque express
-const postController = require("../controllers/post.controller");
+const {
+  setPosts,
+  getPosts,
+  editPost,
+  deletePost,
+  likePost,
+  dislikePost,
+} = require("../controllers/post.controller");
 
 // CRUD = Create, Read, Update, Delete (= get, post, put, delete)
 
@@ -8,32 +15,42 @@ const postController = require("../controllers/post.controller");
 // // res = response = la réponse que nous recevons du serveur
 
 //On crée une route pour récupérer tous les posts
-router.get("/", (req, res) => {
-  res.json({ message: "Voici tous les posts" });
-});
+// router.get("/", (req, res) => {
+//   res.json({ message: "Voici tous les posts" });
+// });
 
-router.post("/", postController.setPosts);
+router.get("/", getPosts);
+
+router.post("/", setPosts);
 
 // On crée une route pour modifier un post (mise à jour)
-router.put("/:id", (req, res) => {
-  // console.log(req.params.id);
-  res.json({ messageId: `Voici le post ${req.params.id}` });
-});
+// router.put("/:id", (req, res) => {
+//   // console.log(req.params.id);
+//   res.json({ messageId: `Voici le post ${req.params.id}` });
+// });
+
+router.put("/:id", editPost);
 
 // On crée une route pour supprimer un post
-router.delete("/:id", (req, res) => {
-  res.json({ message: `Post supprimé avec succès ${req.params.id}` });
-});
+// router.delete("/:id", (req, res) => {
+//   res.json({ message: `Post supprimé avec succès ${req.params.id}` });
+// });
+router.delete("/:id", deletePost);
 
 // On crée une route pour liker un post
-router.patch("/like-post/:id", (req, res) => {
-  res.json({ message: `Post liké: id: ${req.params.id}` });
-});
+// router.patch("/like-post/:id", (req, res) => {
+//   res.json({ message: `Post liké: id: ${req.params.id}` });
+// });
+router.patch("/like-post/:id", likePost);
 
 // On crée une route pour ne pas liker un post
-router.patch("/dislike-post/:id", (req, res) => {
-  res.json({ message: `Post non liké: id: ${req.params.id}` });
-});
+// router.patch("/dislike-post/:id", (req, res) => {
+//   res.json({ message: `Post non liké: id: ${req.params.id}` });
+// });
+
+router.patch("/dislike-post/:id", dislikePost);
 
 //On exporte le router (tout notre projet aura accès à cette route)
 module.exports = router;
+
+
